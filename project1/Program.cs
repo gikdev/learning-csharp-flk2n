f03ff8347ceb28d81a54543189c2fcc6d1ae27d2
gikdev <m.m.bhrm85@gmail.com>
2025-10-26 18:24:21 +0330

    Initial commit.

---
+++ project1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Project1;
+
+internal class Program {
+  static void Main(string[] args) {
+    var name = args[0];
+
+    if (name is null)
+      throw new Exception("Provide a name as an argument plz!");
+
+    Console.WriteLine($"Hello {name}!");
+  }
+}
