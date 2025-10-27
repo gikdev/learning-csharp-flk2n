f857b4629557ceb9f2ee2db03b24328f10b79708
gikdev <m.m.bhrm85@gmail.com>
2025-10-27 14:11:33 +0330

    Finish the "ZTM C#/.NET bootcamp"

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,12 +1,1 @@
-﻿namespace Project1;
-
-internal class Program {
-  static void Main(string[] args) {
-    var name = args[0];
-
-    if (name is null)
-      throw new Exception("Provide a name as an argument plz!");
-
-    Console.WriteLine($"Hello {name}!");
-  }
-}
+﻿
